7918317a93bb88dca533fd446a64ad798dff262c
Michał Wrzosek <michal@wrzosek.pl>
2017-05-01 13:59:59 +0200

    Add enzyme - update simple test for Menu

--- source/containers/Menu.test.js
+++ source/containers/Menu.test.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import expect from 'expect';
-import { renderIntoDocument } from 'react-dom/test-utils';
+import { describe, it } from 'mocha';
+import { shallow } from 'enzyme'
 import Menu from './Menu';
 
 describe('/containers/Menu', () => {
   it('should render', () => {
-    const item = renderIntoDocument(
-      <Menu />
-    );
-
+    const item = shallow(<Menu />);
     expect(item).toExist();
   });
 });
