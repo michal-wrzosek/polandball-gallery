9c7dda46ffcb364ebb47ddb29ee972e630b56402
Michał Wrzosek <michal@wrzosek.pl>
2017-05-04 16:51:24 +0200

    Fixed existing saga tests

--- source/sagas/rootSaga.test.js
+++ source/sagas/rootSaga.test.js
@@ -1,6 +1,10 @@
 import { describe, it } from 'mocha';
 import assert from 'assert';
-import { call, put, takeEvery } from 'redux-saga/effects';
+import {
+  call,
+  put,
+  takeLatest
+} from 'redux-saga/effects';
 import { cloneableGenerator } from 'redux-saga/utils';
 import {
   fetchGalleries,
@@ -21,170 +25,182 @@ import {
 } from './rootSaga';
 
 describe('sagas/rootSaga', () => {
-  describe('watchGetGalleries()', () => {
-    const gen = watchGetGalleries();
+  describe('watchers', () => {
+    describe('watchGetGalleries()', () => {
+      const gen = watchGetGalleries();
 
-    it('should takeEvery GET_GALLERIES actions and call getGalleriesAsync()', () => {
-      assert.deepEqual(
-        gen.next().value,
-        takeEvery(GET_GALLERIES, getGalleriesAsync)
-      );
+      it('should takeLatest GET_GALLERIES actions and call getGalleriesAsync()', () => {
+        assert.deepEqual(
+          gen.next().value,
+          takeLatest(GET_GALLERIES, getGalleriesAsync)
+        );
+      });
+
+      it('should be done', () => {
+        assert.deepEqual(
+          gen.next().done,
+          true
+        );
+      });
     });
 
-    it('should be done', () => {
-      assert.deepEqual(
-        gen.next().done,
-        true
-      );
+    describe('watchSearch()', () => {
+      const gen = watchSearch();
+
+      it('should takeLatest SEARCH actions and call searchAsync()', () => {
+        assert.deepEqual(
+          gen.next().value,
+          takeLatest(SEARCH, searchAsync)
+        );
+      });
+
+      it('should be done', () => {
+        assert.deepEqual(
+          gen.next().done,
+          true
+        );
+      });
     });
   });
 
-  describe('getGalleriesAsync()', () => {
-    const data = {};
-    data.gen = cloneableGenerator(getGalleriesAsync)();
-
-    it('should call fetchGalleries()', () => {
-      assert.deepEqual(
-        data.gen.next().value,
-        call(fetchGalleries)
-      );
-    });
-
-
-    describe('when api works fine', () => {
-      const successfulResponse = {
-        response: {
-          galleries: [
-            {
-              id: 'zzfw',
-              isAlbum: true,
-              coverId: 'zzz',
-              coverWidth: 20,
-              coverHeight: 20,
-              coverUrl: 'http://example.com/zzz.jpg',
-              coverThumbUrl: 'http://example.com/zzzs.jpg',
-            },
-          ],
-        },
+  describe('workers', () => {
+    describe('getGalleriesAsync()', () => {
+      const data = {};
+      const action = {
+        type: GET_GALLERIES,
+        searchPhrase: '',
+        page: 0,
       };
+      data.gen = cloneableGenerator(getGalleriesAsync)(action);
 
-      it('should dispatch getGalleriesSucceeded() action', () => {
-        data.clone = data.gen.clone();
+      it('should call fetchGalleries()', () => {
         assert.deepEqual(
-          data.gen.next(successfulResponse).value,
-          put(getGalleriesSucceeded(successfulResponse.response.galleries))
+          data.gen.next().value,
+          call(fetchGalleries, {
+            searchPhrase: '',
+            page: 0,
+          })
         );
       });
 
-      it('should be done', () => {
-        assert.deepEqual(
-          data.gen.next().done,
-          true
-        );
+
+      describe('when api works fine', () => {
+        const successfulResponse = {
+          response: {
+            galleries: [
+              {
+                id: 'zzfw',
+                isAlbum: true,
+                coverId: 'zzz',
+                coverWidth: 20,
+                coverHeight: 20,
+                coverUrl: 'http://example.com/zzz.jpg',
+                coverThumbUrl: 'http://example.com/zzzs.jpg',
+              },
+            ],
+          },
+        };
+
+        it('should dispatch getGalleriesSucceeded() action', () => {
+          data.clone = data.gen.clone();
+          assert.deepEqual(
+            data.gen.next(successfulResponse).value,
+            put(getGalleriesSucceeded(successfulResponse.response.galleries))
+          );
+        });
+
+        it('should be done', () => {
+          assert.deepEqual(
+            data.gen.next().done,
+            true
+          );
+        });
+      });
+
+      describe('when api throw an error', () => {
+        const errorResponse = { error: {} };
+
+        it('should dispatch getGalleriesFailed() action', () => {
+          assert.deepEqual(
+            data.clone.next(errorResponse).value,
+            put(getGalleriesFailed(errorResponse.error))
+          );
+        });
+
+        it('should be done', () => {
+          assert.deepEqual(
+            data.clone.next().done,
+            true
+          );
+        });
       });
     });
 
-    describe('when api throw an error', () => {
-      const errorResponse = { error: {} };
-
-      it('should dispatch getGalleriesFailed() action', () => {
-        assert.deepEqual(
-          data.clone.next(errorResponse).value,
-          put(getGalleriesFailed(errorResponse.error))
-        );
-      });
-
-      it('should be done', () => {
-        assert.deepEqual(
-          data.clone.next().done,
-          true
-        );
-      });
-    });
-  });
-
-  describe('watchSearch()', () => {
-    const gen = watchSearch();
-
-    it('should takeEvery SEARCH actions and call searchAsync()', () => {
-      assert.deepEqual(
-        gen.next().value,
-        takeEvery(SEARCH, searchAsync)
-      );
-    });
-
-    it('should be done', () => {
-      assert.deepEqual(
-        gen.next().done,
-        true
-      );
-    });
-  });
-
-  describe('searchAsync()', () => {
-    const action = {
-      type: SEARCH,
-      searchPhrase: 'test search',
-    };
-    const data = {};
-    data.gen = cloneableGenerator(searchAsync)(action);
-
-    it('should call fetchGalleries() with searchPrase', () => {
-      assert.deepEqual(
-        data.gen.next().value,
-        call(fetchGalleries, { searchPhrase: 'test search' })
-      );
-    });
-
-
-    describe('when api works fine', () => {
-      const successfulResponse = {
-        response: {
-          galleries: [
-            {
-              id: 'zzfw',
-              isAlbum: false,
-              coverId: 'zzz',
-              coverWidth: 200,
-              coverHeight: 300,
-              coverUrl: 'http://example.com/zzz.jpg',
-              coverThumbUrl: 'http://example.com/zzzs.jpg',
-            },
-          ],
-        },
+    describe('searchAsync()', () => {
+      const action = {
+        type: SEARCH,
+        searchPhrase: 'test search',
       };
+      const data = {};
+      data.gen = cloneableGenerator(searchAsync)(action);
 
-      it('should dispatch searchSucceeded() action', () => {
-        data.clone = data.gen.clone();
+      it('should call fetchGalleries() with searchPrase', () => {
         assert.deepEqual(
-          data.gen.next(successfulResponse).value,
-          put(searchSucceeded(successfulResponse.response.galleries))
+          data.gen.next().value,
+          call(fetchGalleries, { searchPhrase: 'test search' })
         );
       });
 
-      it('should be done', () => {
-        assert.deepEqual(
-          data.gen.next().done,
-          true
-        );
+
+      describe('when api works fine', () => {
+        const successfulResponse = {
+          response: {
+            galleries: [
+              {
+                id: 'zzfw',
+                isAlbum: false,
+                coverId: 'zzz',
+                coverWidth: 200,
+                coverHeight: 300,
+                coverUrl: 'http://example.com/zzz.jpg',
+                coverThumbUrl: 'http://example.com/zzzs.jpg',
+              },
+            ],
+          },
+        };
+
+        it('should dispatch searchSucceeded() action', () => {
+          data.clone = data.gen.clone();
+          assert.deepEqual(
+            data.gen.next(successfulResponse).value,
+            put(searchSucceeded(successfulResponse.response.galleries))
+          );
+        });
+
+        it('should be done', () => {
+          assert.deepEqual(
+            data.gen.next().done,
+            true
+          );
+        });
       });
-    });
 
-    describe('when api throw an error', () => {
-      const errorResponse = { error: {} };
+      describe('when api throw an error', () => {
+        const errorResponse = { error: {} };
 
-      it('should dispatch searchFailed() action', () => {
-        assert.deepEqual(
-          data.clone.next(errorResponse).value,
-          put(searchFailed(errorResponse.error))
-        );
-      });
+        it('should dispatch searchFailed() action', () => {
+          assert.deepEqual(
+            data.clone.next(errorResponse).value,
+            put(searchFailed(errorResponse.error))
+          );
+        });
 
-      it('should be done', () => {
-        assert.deepEqual(
-          data.clone.next().done,
-          true
-        );
+        it('should be done', () => {
+          assert.deepEqual(
+            data.clone.next().done,
+            true
+          );
+        });
       });
     });
   });
